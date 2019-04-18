e0e541c1afeefbfa10f21baae1d964d43ed53ec1
ToKimSeoYeon <rlagmlwnd3@daum.net>
2019-04-18 16:21:09 +0900

    make jwt-token

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,7 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
 import { adjectives, nouns } from "./words";
 import nodemailer from "nodemailer";
 import sgTransport from "nodemailer-sendgrid-transport";
+import jwt from "jsonwebtoken";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
@@ -14,6 +15,7 @@ export const generateSecret = () => {
 const sendMail = email => {
   const options = {
     auth: {
+      api_user: process.env.SENDGRID_USERNAME,
       api_key: process.env.SENGRID_PASSWORD
     }
   };
@@ -23,10 +25,12 @@ const sendMail = email => {
 
 export const sendSecretMail = (adress, secret) => {
   const email = {
-    from: "Timecut",
+    from: "nico@prismagram.com",
     to: adress,
     subject: "🔒Login Secret for Prismagram🔒",
-    html: `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to log in`
+    html: `Hello! Your login secret is <strong>${secret}</strong>.<br/>Copy paste on the app/website to log in`
   };
   return sendMail(email);
 };
+
+export const generateToken = id => jwt.sign({ id }, process.env.JWT_SECRET);
